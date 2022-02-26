3fdccb5e6c090e43e780982b9a519417d3bae847
jvdionizio <dionizio145@gmail.com>
2022-02-26 02:26:15 -0300

    Added new feature preview card

--- src/App.js
+++ src/App.js
@@ -2,27 +2,70 @@ import React from 'react';
 import Card from './components/Card';
 import Form from './components/Form';
 
-const defaultProps = {
-  cardName: 'Nome da carta',
-  cardDescription: 'Descrição da carta',
-  cardAttr1: '12',
-  cardAttr2: '34',
-  cardAttr3: '56',
-  cardImage: 'url-to-image',
-  cardRare: 'raro',
-  cardTrunfo: true,
-  hasTrunfo: false,
-  isSaveButtonDisabled: false,
-  onInputChange: () => {},
-  onSaveButtonClick: () => {},
-};
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.handleChange = this.handleChange.bind(this);
+    this.state = {
+      name: 'Nome da carta',
+      description: 'Descrição da carta',
+      attr1: '00',
+      attr2: '00',
+      attr3: '00',
+      image: 'url-to-image',
+      rare: 'raro',
+      trunfo: false,
+      btnDisable: true,
+    };
+  }
+
+  handleChange({ target }) {
+    const { name } = target;
+    const value = target.type === 'checkbox' ? target.checked : target.value;
+    this.setState({
+      [name]: value,
+    });
+  }
+
   render() {
+    const {
+      name,
+      description,
+      attr1,
+      attr2,
+      attr3,
+      image,
+      rare,
+      trunfo,
+      btnDisable } = this.state;
     return (
       <div>
         <div className="makingCard">
-          <Form { ...defaultProps } />
-          <Card { ...defaultProps } />
+          <Form
+            cardName={ name }
+            cardDescription={ description }
+            cardAttr1={ attr1 }
+            cardAttr2={ attr2 }
+            cardAttr3={ attr3 }
+            cardImage={ image }
+            cardRare={ rare }
+            cardTrunfo={ trunfo }
+            // hasTrunfo= false
+            isSaveButtonDisabled={ btnDisable }
+            onInputChange={ this.handleChange }
+          />
+          <Card
+            cardName={ name }
+            cardDescription={ description }
+            cardAttr1={ attr1 }
+            cardAttr2={ attr2 }
+            cardAttr3={ attr3 }
+            cardImage={ image }
+            cardRare={ rare }
+            cardTrunfo={ trunfo }
+            // hasTrunfo= false
+            isSaveButtonDisabled={ btnDisable }
+          />
         </div>
       </div>
     );
